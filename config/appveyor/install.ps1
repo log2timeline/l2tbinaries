be984199cf50156a0ae17b0d3ba78aa6ba01ec1e
Joachim Metz <joachim.metz@gmail.com>
2017-03-08 07:44:27 +0100

    Worked on tests.

---
+++ config/appveyor/install.ps1
@@ -0,0 +1,20 @@
+# Script to set up tests on AppVeyor Windows.
+
+$Output = Invoke-Expression -Command "git clone https://github.com/log2timeline/l2tdevtools.git ..\l2tdevtools 2>&1" | %{ "$_" }
+Write-Host (${Output} | Out-String)
+
+If ($env:APPVEYOR_REPO_BRANCH -eq "main")
+{
+	$Track = "stable"
+}
+Else
+{
+	$Track = $env:APPVEYOR_REPO_BRANCH
+}
+New-Item -ItemType "directory" -Name "dependencies"
+
+$env:PYTHONPATH = "..\l2tdevtools"
+
+$Output = Invoke-Expression -Command "& '${env:PYTHON}\python.exe' ..\l2tdevtools\tools\update.py --download-directory dependencies --machine-type ${env:MACHINE_TYPE} --track ${Track} 2>&1" | %{ "$_" }
+Write-Host (${Output} | Out-String)
+
